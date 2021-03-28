782ace13a2e089f84382d1f41e7c0178599780d2
Itilon <ivanov.elect@gmail.com>
2021-03-29 00:11:03 +0300

    Intersection observer and interview link added to about page

---
+++ js/about.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const messageWrapper = document.querySelector('.message-wrapper');
+    const messageTitle = document.querySelector('.message-wrapper h3');
+    const message = document.querySelector('.message-wrapper iframe');
+
+    let firstIntersection = false;
+
+    const onIntersection = (entries) => {
+        if (!firstIntersection) {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    messageTitle.classList.add('top-slide');
+                    message.classList.add('bottom-slide');
+                    firstIntersection = true;
+                }
+            });
+        }
+    };
+
+    const intersectionOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+    };
+      
+    const observer = new IntersectionObserver(onIntersection, intersectionOptions);
+    observer.observe(messageWrapper);
+});
