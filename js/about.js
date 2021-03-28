bfb53c6de96d316ef1dc76374aee5cbc8681e93b
Itilon <ivanov.elect@gmail.com>
2021-03-29 00:22:10 +0300

    IntersectionObserver support partial check and fallback

--- js/about.js
+++ js/about.js
@@ -3,26 +3,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const messageTitle = document.querySelector('.message-wrapper h3');
     const message = document.querySelector('.message-wrapper iframe');
 
-    let firstIntersection = false;
+    if ('IntersectionObserver' in window) {
+        let firstIntersection = false;
 
-    const onIntersection = (entries) => {
-        if (!firstIntersection) {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    messageTitle.classList.add('top-slide');
-                    message.classList.add('bottom-slide');
-                    firstIntersection = true;
-                }
-            });
-        }
-    };
+        const onIntersection = (entries) => {
+            if (!firstIntersection) {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        messageTitle.classList.add('top-slide');
+                        message.classList.add('bottom-slide');
+                        firstIntersection = true;
+                    }
+                });
+            }
+        };
 
-    const intersectionOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 1.0
-    };
-      
-    const observer = new IntersectionObserver(onIntersection, intersectionOptions);
-    observer.observe(messageWrapper);
+        const intersectionOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1.0
+        };
+        
+        const observer = new IntersectionObserver(onIntersection, intersectionOptions);
+        observer.observe(messageWrapper);  
+    } else {
+        messageTitle.classList.add('top-slide');
+        message.classList.add('bottom-slide');
+    }
 });
