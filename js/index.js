5caabb61f4349a9aec28a34ea0111b1546c81374
Itilon <ivanov.elect@gmail.com>
2021-04-11 22:39:23 +0300

    Index page subscriber form initial setup

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const showcaseForm = document.querySelector('.showcase-form form');
+
+    showcaseForm.addEventListener('submit', submitSubscriberForm.bind(null, showcaseForm));
 });
+
+const submitSubscriberForm = (form) => {
+    this.event.preventDefault();
+    if (form.checkValidity()) {
+        sendFormData(form)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error();
+                }
+            })
+            .catch(() => {
+                
+            });
+    } else {
+        
+    }
+};
