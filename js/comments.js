7febdd78f1ab90e9de25d3b456fcd8442dae17ed
Itilon <ivanov.elect@gmail.com>
2021-04-28 13:34:18 +0300

    Feedback form toggle additional code

--- js/comments.js
+++ js/comments.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const feedbackBtnContainer = document.querySelector('.feedback-btn-container');
     const feedbackBtn = feedbackBtnContainer.querySelector('button');
     const feedbackForm = document.querySelector('.feedback-form');
+    const exitBtn = feedbackForm.querySelector('.exit-btn')
 
-    feedbackBtn.addEventListener('click', onFeedbackButtonClick.bind(null, feedbackBtnContainer, feedbackForm));
+    feedbackBtn.addEventListener('click', toggleFeedbackForm.bind(null, feedbackBtnContainer, feedbackForm));
+    exitBtn.addEventListener('click', toggleFeedbackForm.bind(null, feedbackBtnContainer, feedbackForm));
 });
 
-const onFeedbackButtonClick = (feedbackBtnContainer, feedbackForm) => {
-    feedbackBtnContainer.classList.add('hidden');
-    feedbackForm.classList.remove('hidden');
+const toggleFeedbackForm = (feedbackBtnContainer, feedbackForm) => {
+    feedbackBtnContainer.classList.toggle('hidden');
+    feedbackForm.classList.toggle('hidden');
 };
