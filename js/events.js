43559e33ffb8e534dc7bffdb9c7b29f893968dc2
Itilon <ivanov.elect@gmail.com>
2021-05-07 12:26:37 +0300

    Events form validation initial code

--- js/events.js
+++ js/events.js
@@ -15,10 +15,22 @@ const openSignupForm = (button) => {
     const select = signupFormContainer.querySelector('select');
     select.disabled = true;
 
+    const inputs = signupFormContainer.querySelectorAll('input');
+    inputs.forEach((input) => {
+        input.addEventListener('keyup', checkForInvalidBorderAndErrorMessage.bind(null, input));
+        input.addEventListener('focusout', checkElementValidity.bind(null, input));
+    })
+
     signupFormContainer.classList.add('visible');
 
     darkOverlay.addEventListener('click', () => {
         signupFormContainer.classList.remove('visible');
         removeHTMLElement(darkOverlay);
     });
+};
+
+const checkForInvalidBorderAndErrorMessage = (input) => {
+    if (input.checkValidity()) {
+        input.classList.remove('invalid');
+    }
 };
