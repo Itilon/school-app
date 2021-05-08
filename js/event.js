2807b7760ee63fae629b5a4966d46ece9106958a
Itilon <ivanov.elect@gmail.com>
2021-05-08 16:26:59 +0300

    Dark overlay added to event page

--- js/event.js
+++ js/event.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgContainer = document.querySelector('.img-container');
-    const rightArrow = document.querySelector('.fa-angle-right');
-    const leftArrow = document.querySelector('.fa-angle-left');
+    const leftSlider = document.querySelector('.left-slider');
+    const leftSliderImages = leftSlider.querySelectorAll('.gallery-img');
+    const rightArrow = leftSlider.querySelector('.fa-angle-right');
+    const leftArrow = leftSlider.querySelector('.fa-angle-left');
+
+    leftSliderImages.forEach((image) => image.addEventListener('click', showFullScreenImage));
 });
+
+const showFullScreenImage = () => {
+    const darkOverlay = createHTMLElement('div', 'overlay', null);
+    document.body.prepend(darkOverlay);
+
+    darkOverlay.addEventListener('click', closeFullScreenImage.bind(null, darkOverlay));
+};
+
+const closeFullScreenImage = (darkOverlay) => {
+    removeHTMLElement(darkOverlay);
+};
