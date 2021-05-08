437e49c24a90bbff23a47c7a118454360237cd80
Itilon <ivanov.elect@gmail.com>
2021-05-08 15:18:19 +0300

    Event page gallery initial code

---
+++ js/event.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgContainer = document.querySelector('.img-container');
+    const rightArrow = document.querySelector('.fa-angle-right');
+    const leftArrow = document.querySelector('.fa-angle-left');
+});
