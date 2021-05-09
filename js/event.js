81844e42dfca8cc9c7b4d444046d5371caa7e2e7
Itilon <ivanov.elect@gmail.com>
2021-05-09 23:19:40 +0300

    Event gallery slider initial functionality

--- js/event.js
+++ js/event.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const leftSlider = document.querySelector('.left-slider');
+    const leftsliderImageContainers = leftSlider.querySelectorAll('.image-container');
     const leftSliderImages = leftSlider.querySelectorAll('.gallery-img');
     const rightArrow = leftSlider.querySelector('.fa-angle-right');
     const leftArrow = leftSlider.querySelector('.fa-angle-left');
 
     leftSliderImages.forEach((image) => image.addEventListener('click', showFullScreenImage));
+    rightArrow.addEventListener('click', slideToTheRight.bind(null, [...leftsliderImageContainers]));
+    leftArrow.addEventListener('click', slideToTheLeft.bind(null, [...leftsliderImageContainers]));
 });
 
 const showFullScreenImage = () => {
@@ -16,4 +19,34 @@ const showFullScreenImage = () => {
 
 const closeFullScreenImage = (darkOverlay) => {
     removeHTMLElement(darkOverlay);
+};
+
+const slideToTheRight = (containers) => {
+    const previousContainer = containers.find(container => container.classList.contains('previous-container'));
+    const currentContainer = containers.find(container => container.classList.contains('current-container'));
+    const nextContainer = containers.find(container => container.classList.contains('next-container'));
+
+    previousContainer.classList.remove('previous-container');
+    previousContainer.classList.add('current-container');
+    
+    currentContainer.classList.remove('current-container');
+    currentContainer.classList.add('next-container');
+
+    nextContainer.classList.remove('next-container');
+    nextContainer.classList.add('previous-container');
+};
+
+const slideToTheLeft = (containers) => {
+    const previousContainer = containers.find(container => container.classList.contains('previous-container'));
+    const currentContainer = containers.find(container => container.classList.contains('current-container'));
+    const nextContainer = containers.find(container => container.classList.contains('next-container'));
+
+    previousContainer.classList.remove('previous-container');
+    previousContainer.classList.add('next-container');
+    
+    currentContainer.classList.remove('current-container');
+    currentContainer.classList.add('previous-container');
+
+    nextContainer.classList.remove('next-container');
+    nextContainer.classList.add('current-container');
 };
