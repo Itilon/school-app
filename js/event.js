4dbb6359a5b7ca1b8b1b54afd631afa47eb4c148
Itilon <ivanov.elect@gmail.com>
2021-05-16 18:23:30 +0300

    Right slider initial functionality

--- js/event.js
+++ js/event.js
@@ -4,13 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const leftSliderImages = leftSlider.querySelectorAll('.gallery-img');
     const rightSlider = document.querySelector('.right-slider');
     const rightSliderImageContainers = rightSlider.querySelectorAll('.right-container');
-    const rightSliderImages = rightSlider.querySelectorAll('.right-img');
     const rightArrow = leftSlider.querySelector('.fa-angle-right');
     const leftArrow = leftSlider.querySelector('.fa-angle-left');
 
-    leftSliderImages.forEach((image) => image.addEventListener('click', showFullScreenImage.bind(null, image, leftSlider, rightSlider)));
-    rightArrow.addEventListener('click', slideToTheRight.bind(null, [...leftSliderImageContainers], [...rightSliderImageContainers]));
-    leftArrow.addEventListener('click', slideToTheLeft.bind(null, [...leftSliderImageContainers], [...rightSliderImageContainers]));
+    leftSliderImages.forEach((image) => {
+        image.addEventListener('click', showFullScreenImage.bind(null, image, leftSlider, rightSlider))
+    });
+    rightSliderImageContainers.forEach((container) => {
+        container.addEventListener('click', () => {
+            if (container.classList.contains('previous')) {
+                slideDown([...leftSliderImageContainers], [...rightSliderImageContainers]);
+            } else if (container.classList.contains('next')) {
+                slideUp([...leftSliderImageContainers], [...rightSliderImageContainers]);
+            }
+        });
+    });
+    rightArrow.addEventListener('click', slideRight.bind(null, [...leftSliderImageContainers], [...rightSliderImageContainers]));
+    leftArrow.addEventListener('click', slideLeft.bind(null, [...leftSliderImageContainers], [...rightSliderImageContainers]));
 });
 
 const showFullScreenImage = (image, leftSlider, rightSlider) => {
@@ -36,7 +46,7 @@ const closeFullScreenImage = (image, leftSlider, rightSlider, darkOverlay) => {
     }, 500);
 };
 
-const slideToTheRight = (leftSliderContainers, rightSliderContainers) => {
+const slideRight = (leftSliderContainers, rightSliderContainers) => {
     const previousContainer = leftSliderContainers.find(container => container.classList.contains('previous-container'));
     const currentContainer = leftSliderContainers.find(container => container.classList.contains('current-container'));
     const nextContainer = leftSliderContainers.find(container => container.classList.contains('next-container'));
@@ -54,7 +64,7 @@ const slideToTheRight = (leftSliderContainers, rightSliderContainers) => {
     changeClass(nextRightSliderContainer, 'next', 'current');
 };
 
-const slideToTheLeft = (leftSliderContainers, rightSliderContainers) => {
+const slideLeft = (leftSliderContainers, rightSliderContainers) => {
     const previousContainer = leftSliderContainers.find(container => container.classList.contains('previous-container'));
     const currentContainer = leftSliderContainers.find(container => container.classList.contains('current-container'));
     const nextContainer = leftSliderContainers.find(container => container.classList.contains('next-container'));
@@ -70,6 +80,14 @@ const slideToTheLeft = (leftSliderContainers, rightSliderContainers) => {
 
     changeClass(nextContainer, 'next-container', 'previous-container');
     changeClass(nextRightSliderContainer, 'next', 'previous');
+};
+
+const slideDown = (leftSliderContainers, rightSlidercontainers) => {
+
+};
+
+const slideUp = (leftSliderContainers, rightSlidercontainers) => {
+
 };
 
 const changeClass = (element, oldClass, newClass) => {
