14d7cd3a1dd058e32342da2d522368ee1ba17367
Itilon <ivanov.elect@gmail.com>
2021-05-29 17:59:16 +0300

    Value cards flipping option added

--- js/about.js
+++ js/about.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const messageWrapper = document.querySelector('.message-wrapper');
     const messageTitle = document.querySelector('.message-wrapper h3');
     const message = document.querySelector('.message-wrapper iframe');
+    const valueCards = document.querySelectorAll('.value-card');
 
     if ('IntersectionObserver' in window) {
         let firstIntersection = false;
@@ -25,4 +26,10 @@ document.addEventListener('DOMContentLoaded', () => {
         messageTitle.classList.add('top-slide');
         message.classList.add('bottom-slide');
     }
+
+    valueCards.forEach((valueCard) => valueCard.addEventListener('click', flipValueCard.bind(null, valueCard)));
 });
+
+const flipValueCard = (card) => {
+    card.classList.toggle('flipped');
+};
