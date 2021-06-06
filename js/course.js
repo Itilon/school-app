42409aae506bd61afb2682ee7ef3496f6827c572
Itilon <ivanov.elect@gmail.com>
2021-06-06 13:21:46 +0300

    Single course consultancy signup form update

--- js/course.js
+++ js/course.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const signupBtn = document.querySelector('.single-course-wrapper .btn');
+    const signupFormContainer = document.querySelector('.signup-form-container');
 
-    signupBtn.addEventListener('click', openSignupForm);
+    signupBtn.addEventListener('click', openSignupForm.bind(null, signupFormContainer));
 });
 
-const openSignupForm = () => {
+const openSignupForm = (signupFormContainer) => {
     const darkOverlay = createHTMLElement('div', 'overlay', null);
     const closingBtn = createHTMLElement('span', 'closing-btn', '&#10005');
     darkOverlay.appendChild(closingBtn);
     document.body.prepend(darkOverlay);
 
-    darkOverlay.addEventListener('click', closeSignupForm.bind(null, darkOverlay));
+    signupFormContainer.classList.add('visible');
+
+    darkOverlay.addEventListener('click', closeSignupForm.bind(null, darkOverlay, signupFormContainer));
 };
 
-const closeSignupForm = (darkOverlay) => {
+const closeSignupForm = (darkOverlay, signupFormContainer) => {
+    signupFormContainer.classList.remove('visible');
     removeHTMLElement(darkOverlay);
 };
