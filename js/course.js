12071e0ca617499c11f74af89483b04e5e2759d9
Itilon <ivanov.elect@gmail.com>
2021-06-06 13:32:01 +0300

    Single course consultancy signup form additional code

--- js/course.js
+++ js/course.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const signupBtn = document.querySelector('.single-course-wrapper .btn');
     const signupFormContainer = document.querySelector('.signup-form-container');
+    const inputs = signupFormContainer.querySelectorAll('input');
 
-    signupBtn.addEventListener('click', openSignupForm.bind(null, signupFormContainer));
+    signupBtn.addEventListener('click', openSignupForm.bind(null, signupBtn, signupFormContainer, inputs));
 });
 
-const openSignupForm = (signupFormContainer) => {
+const openSignupForm = (button, signupFormContainer, inputs) => {
     const darkOverlay = createHTMLElement('div', 'overlay', null);
     const closingBtn = createHTMLElement('span', 'closing-btn', '&#10005');
     darkOverlay.appendChild(closingBtn);
     document.body.prepend(darkOverlay);
+
+    const select = signupFormContainer.querySelector('select');
+    const options = signupFormContainer.querySelectorAll('option');
+
+    select.disabled = true;
+    [...options].find((option) => option.value === button.dataset.event).selected = 'selected';
 
     signupFormContainer.classList.add('visible');
 
